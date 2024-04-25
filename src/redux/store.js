7a54d49914c582465100cda4588b710a0eec20a6
a.panasenko <andrii.panasenko295@gmail.com>
2024-04-25 21:39:57 +0300

    add ignoreActions middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { unsplashReducer } from "./unsplashSlice";
-
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 
 export const store = configureStore({
   reducer: {
     gallery: unsplashReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
