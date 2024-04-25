e41e7157303139e8eeccf7aa3f6f35afa9fed7d6
a.panasenko <andrii.panasenko295@gmail.com>
2024-04-25 17:56:30 +0300

    Create MainComponent.js

---
+++ src/components/MainComponent/MainComponent.js
@@ -0,0 +1,29 @@
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { useDispatch } from "react-redux";
+
+export const MainComponent = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+
+    }, [])
+    
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+
